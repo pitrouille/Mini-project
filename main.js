f5d01c29ed068cc15484e6f9070f5dd746da1840
Chloé <chloedrieux67@gmail.com>
2024-03-25 12:07:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 class toDo{
+    // Notre constructeur
     constructor(content, statut){
         this.content = content
         this.statut = statut
     }
+    // Récupère notre Todo
     getTodo(){
         if (this.statut){
-            console.log('oui')
             return `<ul>${this.content}<input type="checkbox" id="todoTrue" checked></input id="delTodo><button id="delTodo">Suprimer</button></ul>`
         }else{
-            console.log('non')
             return `<ul>${this.content}<input type="checkbox" id="todoFalse"></input><button id="delTodo">Suprimer</button></ul>`
         }
         
@@ -16,16 +16,29 @@ class toDo{
     
 }
 
+// const bTodo = document.querySelector('#createTodo')
+// bTodo.addEventListener("click", () => {
+//     const createTodo = document.querySelector('#contentTodo')
+//     const newTodo = new toDo(createTodo.value, false)
+//     console.log(newTodo.getTodo())
+//     const listTodo = document.querySelector('#todoList')
+//     listTodo.innerHTML = listTodo.innerHTML + newTodo.getTodo()
+//     const delButtons = document.querySelectorAll('button')
+//     delButtons.forEach(delButton => delButton.addEventListener('click', () => {
+//         delButton.parentElement.innerHTML = ""
+//     }))
+// })
+
 const bTodo = document.querySelector('#createTodo')
 bTodo.addEventListener("click", () => {
     const createTodo = document.querySelector('#contentTodo')
     const newTodo = new toDo(createTodo.value, false)
-    console.log(newTodo.getTodo())
+    localStorage.setItem('todo', newTodo)
     const listTodo = document.querySelector('#todoList')
     listTodo.innerHTML = listTodo.innerHTML + newTodo.getTodo()
     const delButtons = document.querySelectorAll('button')
     delButtons.forEach(delButton => delButton.addEventListener('click', () => {
         delButton.parentElement.innerHTML = ""
+        localStorage.removeItem(createTodo.value)
     }))
-    
 })
